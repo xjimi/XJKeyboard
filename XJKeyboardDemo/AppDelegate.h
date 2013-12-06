e459347cdd8f567f574276da4088bd961f95f898
xjimi <fn5128@gmail.com>
2013-12-06 17:11:22 +0800

    Initial Commit

---
+++ XJKeyboardDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  XJKeyboardDemo
+//
+//  Created by YU-HENG WU on 2013/12/6.
+//  Copyright (c) 2013年 xjimi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ XJKeyboardDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  XJKeyboardDemo
+//
+//  Created by YU-HENG WU on 2013/12/6.
+//  Copyright (c) 2013年 xjimi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
